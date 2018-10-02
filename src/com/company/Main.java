e5cf8165cbba4738a7b0339e7d5bcd935888c2ac
Ekaterina Soymonova <Ekaterina.SOYMONOVA@raiffeisen.ru>
2018-10-02 10:26:38 +0300

    Start project: Sweets

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
 
         Marmelad marmelad = new Marmelad();
         marmelad.name = "PinkBubble";
